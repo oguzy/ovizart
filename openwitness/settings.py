e0accce119ba2bdb52275caab6e2418e5177e6f4
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-04-25 21:27:04 +0300

    mongodb engine for Django is set

--- openwitness/settings.py
+++ openwitness/settings.py
@@ -113,12 +113,12 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djangotoolbox',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
