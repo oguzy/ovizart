8365837dbeda92c5bcbe44c7b50d375cd8355f27
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-04-25 21:27:31 +0300

    Urls for admin interface is set

--- openwitness/urls.py
+++ openwitness/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
