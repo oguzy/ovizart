d1e4b9a2ec7137a2214a8584be420f1b3c8e032f
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-04-26 23:12:32 +0300

    basi url definition
    homepage is opened with the bootstrap design

--- openwitness/urls.py
+++ openwitness/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     # (r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
+    url(r'^$', view='main.views.welcome', name='home_page'),
+    (r'^pcap/', include('pcap.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -17,9 +19,10 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
 
+
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT}),
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+             {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     )
 
