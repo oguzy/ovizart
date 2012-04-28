cf49b50822951d97ab7d6eccc8457471f7a0b8a8
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-04-28 22:35:35 +0300

    template directory fix

--- openwitness/settings.py
+++ openwitness/settings.py
@@ -113,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_NAME, "templates"),
+    os.path.join(PROJECT_ROOT, "templates"),
 )
 
 INSTALLED_APPS = (
