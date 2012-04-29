74ed5ca86b9939ef1029f6541058094726d00e05
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-04-29 17:08:08 +0300

    Uploaded file name fixed
    Also added log messages

--- openwitness/modules/file/handler.py
+++ openwitness/modules/file/handler.py
@@ -4,19 +4,26 @@
 import datetime
 import os
 from django.conf import settings
+from openwitness.modules.traffic.log.logger import Logger
 
 class Handler:
     def __init__(self):
+        self.file_path = None
+        log = Logger("File Handler", "DEBUG")
         now = datetime.datetime.now()
+        log.message("Now is: %s:" % now)
         directory_name = now.strftime("%d-%m-%y")
-        directory_path = "/".join([settings.PROJECT_ROOT, directory_name])
+        log.message("Directory name: %s:" % directory_name)
+        directory_path = "/".join([settings.PROJECT_ROOT, "uploads", directory_name])
+        log.message("Directory path: %s" % directory_path)
         if not os.path.exists(directory_path):
             os.mkdir(directory_path)
+            log.message("Directory created")
         self.upload_dir = directory_path
 
     def save_file(self, f):
-        self.file_path = self.upload_dir + f.name
-        destination = open(file_path, 'wb+')
+        self.file_path = "/".join([self.upload_dir, f.name])
+        destination = open(self.file_path, 'wb+')
         for chunk in f.chunks():
             destination.write(chunk)
         destination.close()
