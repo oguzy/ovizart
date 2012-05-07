42bf0fd3d9ba23fde9843c9060cd7ec766e72495
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-05-07 15:58:08 +0300

    a seperate function is defined for directory creation

--- openwitness/modules/file/handler.py
+++ openwitness/modules/file/handler.py
@@ -3,6 +3,7 @@
 
 import datetime
 import os
+import dpkt
 from django.conf import settings
 from openwitness.modules.traffic.log.logger import Logger
 
@@ -10,16 +11,18 @@ class Handler:
     def __init__(self):
         self.file_path = None
         self.file_name = None
-        log = Logger("File Handler", "DEBUG")
+        self.log = Logger("File Handler", "DEBUG")
+
+    def create_dir(self):
         now = datetime.datetime.now()
-        log.message("Now is: %s:" % now)
+        self.log.message("Now is: %s:" % now)
         directory_name = now.strftime("%d-%m-%y")
-        log.message("Directory name: %s:" % directory_name)
+        self.log.message("Directory name: %s:" % directory_name)
         directory_path = "/".join([settings.PROJECT_ROOT, "uploads", directory_name])
-        log.message("Directory path: %s" % directory_path)
+        self.log.message("Directory path: %s" % directory_path)
         if not os.path.exists(directory_path):
             os.mkdir(directory_path)
-            log.message("Directory created")
+            self.log.message("Directory created")
         self.upload_dir = directory_path
 
     def save_file(self, f):
