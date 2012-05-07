70560ada4b5726fe98d59dab4c4bdd2a6b89783b
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-05-07 16:01:18 +0300

    fixes at the model definition
    some fields name are shorteneds and foreign key fixes

--- openwitness/pcap/models.py
+++ openwitness/pcap/models.py
@@ -3,23 +3,27 @@ from djangotoolbox.fields import EmbeddedModelField, ListField
 
 # Create your models here.
 class Flow(models.Model):
-    file_name = models.CharField(max_length=100)
+    hash_value = models.CharField(max_length=50)
+    file_name = models.CharField(max_length=50)
     path = models.FilePathField()
     pcaps = ListField(EmbeddedModelField('Pcap', null=True, blank=True))
 
 class Pcap(models.Model):
+    hash_value = models.CharField(max_length=100)
     file_name = models.FileField(upload_to="uploads", null=True, blank=True)
     path = models.FilePathField()
-    packets = EmbeddedModelField('PacketDetails', null=True, blank=True)
+    packets = ListField(EmbeddedModelField('PacketDetails', null=True, blank=True))
+
+# there should be also a table of fields that kepts the traffic bytes related with communication
 
 class PacketDetails(models.Model):
     #datetime.datetime.fromtimestamp(float("1286715787.71")).strftime('%Y-%m-%d %H:%M:%S')
     timestamp = models.DateTimeField()
     protocol = models.CharField(max_length=10)
-    source_ip = models.IPAddressField()
-    destionation_ip = models.IPAddressField()
-    source_port = models.IntegerField()
-    destionation_port = models.IntegerField()
+    src_ip = models.IPAddressField()
+    dst_ip = models.IPAddressField()
+    sport = models.IntegerField()
+    dport = models.IntegerField()
     http = EmbeddedModelField('HttpDetails', null=True, blank=True)
 
 class HttpDetails(models.Model):
