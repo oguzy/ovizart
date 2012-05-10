5a03e09e065434ffa71685b59ff0faf562c2ca6d
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-05-11 00:15:00 +0300

    saving to tcp/ip data is finished
    http information is gathered as dictionaties. The dictionary data is saved also

--- openwitness/pcap/views.py
+++ openwitness/pcap/views.py
@@ -12,7 +12,7 @@ from openwitness.modules.traffic.parser.tcp.handler import Handler as TcpHandler
 from openwitness.modules.traffic.parser.http.handler import Handler as HttpHandler
 from openwitness.modules.md5.handler import Handler as HashHandler
 
-from openwitness.pcap.models import Flow, Pcap, PacketDetails
+from openwitness.pcap.models import Flow, Pcap, PacketDetails, HttpDetails
 
 from openwitness.modules.traffic.log.logger import Logger
 
@@ -34,20 +34,20 @@ def upload(request):
             context['upload_status'] = True
 
             #save the file name to the db
-            pcap_name = mem_file
+            pcap_name = mem_file.name
             upload_path = file_handler.upload_dir
             # evey pcap file is saved as a flow container, there may or may not be flows, the pcaps colon will give the flow pcaps
             hash_handler = HashHandler()
-            hash_handler.set_file("/".join([mem_file.name, upload_path]))
+            hash_handler.set_file("/".join([pcap_name, upload_path]))
             flow_file, created = Flow.objects.get_or_create(hash_value=hash_handler.get_hash(),file_name=mem_file.name, path=upload_path)
             # send the file to the defined protocol handler so that it can detect
             protocol_handler = settings.PROTOCOL_HANDLER
             package = "openwitness.modules.traffic.detector"
             module_name = ".".join([package, protocol_handler])
             # from openwitness.modules.traffic.detector.x import handler as imported_module
-            imported_module = getattr(__import__(module_name, fromlist=["handler"]), "handler")
-            imported_handler = imported_module.Handler()
-            output = imported_handler.detect_proto(file_handler.file_path, upload_path)
+            traffic_detector_module = getattr(__import__(module_name, fromlist=["handler"]), "handler")
+            traffic_detector_handler = traffic_detector_module.Handler()
+            output = traffic_detector_handler.detect_proto(file_handler.file_path, upload_path)
             log.message("protocol detected: %s" % output)
             if "tcp" in output:
                 # run tcp flow extractor
@@ -74,33 +74,57 @@ def upload(request):
                     packets  = []
                     # better to save tcp level information to db here
                     full_path = "/".join([upload_path, f])
-                    print full_path
                     p_read_handler.open_file(full_path)
                     p_read_handler.open_pcap()
                     pcap = p_read_handler.get_pcap()
+                    # the list that will keep the tcp part of the packet
+                    tcp_list = []
                     for ts, buf in pcap:
                         tcp_handler = TcpHandler()
                         tcp = tcp_handler.read_tcp(ts, buf)
-                        if not tcp: continue
-                        tcp_packet = PacketDetails.objects.create(timestamp=tcp_handler.timestamp, protocol=tcp_handler.proto, src_ip=tcp_handler.src_ip, dst_ip=tcp_handler.dst_ip, sport=tcp_handler.sport, dport=tcp_handler.dport)
-                        packets.append(tcp_packet)
+                        # this list will be used at the layers above tcp
+                        if tcp:
+                            tcp_list.append((tcp, tcp_handler.ident))
+                        else: continue
+                        PacketDetails.objects.create(ident=tcp_handler.ident, timestamp=tcp_handler.timestamp, protocol=tcp_handler.proto, src_ip=tcp_handler.src_ip, dst_ip=tcp_handler.dst_ip, sport=tcp_handler.sport, dport=tcp_handler.dport)
+                    packets = PacketDetails.objects.all()
                     hash_handler.set_file("/".join([f, upload_path]))
+                    # get the pcap object
                     p = Pcap.objects.get(hash_value=hash_handler.get_hash())
-                    p.packets = packets
+                    log.message("pcap for packet update detected: %s" % p)
+                    # update its packets
+                    p.packets = list(packets) # converting a queryset to list
                     p.save()
 
-                    output = imported_handler.detect_appproto(f, upload_path)
+                    output = traffic_detector_handler.detect_appproto(f, upload_path)
                     log.message("protocol detected: %s" % output)
-                    if output.lower() == "http":
+                    if output.strip().lower() == "http":
+                        print "tcp_list", tcp
                         # by looking at the output http hook the parser related with it
-                        #modules/traffic/parser/http
-                        package = "openwitness.modules.traffic.parser"
-                        module_name = ".".join([package, "http"])
-                        # from openwitness.modules.traffic.parser.http import handler as imported_module
-                        imported_module = getattr(__import__(module_name, fromlist=["handler"]), "handler")
-                        imported_handler = imported_module.Handler()
+                        http_handler = HttpHandler()
 
-            #save the returned information at the db also
+                        if tcp_list:
+                            for tcp in tcp_list:
+                                # get the http information to a list
+                                http_info = http_handler.read_http(tcp[0])
+                        # save the result of the http infor to db
+                                http = None
+                                if http_info and http_info.has_key('request'):
+                                    # {'method': request.method, 'uri': request.uri, 'headers': request.headers, 'version': request.version}
+                                    info = http_info['request']
+                                    http = HttpDetails.objects.create(http_type="request", method=info['method'], uri=info['uri'], headers=info['headers'], version=info['version'])
+                                if http_info and http_info.has_key('response'):
+                                    #{'headers': response.headers, 'status': response.status, 'body': response.body, 'version': response.version}
+                                    info = http_info['response']
+                                    http = HttpDetails.objects.create(http_type="response", headers=info['headers'], status=info['status'], body=info['body'], version=info['version'])
+                                # save the packet http information
+                                tcp_packet = filter(lambda x: x.ident == tcp[1], p.packets)[0]
+                                if not http:
+                                    tcp_packet.http = http
+                                    tcp_packet.save()
+
+                    p_read_handler.close_file()
+
     else:
         form = UploadPcapForm()
         context['form'] = form
