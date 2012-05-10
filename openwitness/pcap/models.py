1eae05d6546ef66aa1fb453c899dbb76fcb95ec5
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-05-11 00:10:08 +0300

    added some fixes to the model definitions
    Wrong fields are fixed. Unicode function is defined for human readable console output.
    Ident field is added to PacketDetails to keep the ip id

--- openwitness/pcap/models.py
+++ openwitness/pcap/models.py
@@ -8,37 +8,47 @@ class Flow(models.Model):
     path = models.FilePathField()
     pcaps = ListField(EmbeddedModelField('Pcap', null=True, blank=True))
 
+    def __unicode__(self):
+        return u'%s/%s' % (self.path, self.file_name)
+
 class Pcap(models.Model):
     hash_value = models.CharField(max_length=100)
     file_name = models.FileField(upload_to="uploads", null=True, blank=True)
     path = models.FilePathField()
     packets = ListField(EmbeddedModelField('PacketDetails', null=True, blank=True))
 
+    def __unicode__(self):
+        return u'%s/%s' % (self.path, self.file_name)
+
 # there should be also a table of fields that kepts the traffic bytes related with communication
 
 class PacketDetails(models.Model):
     #datetime.datetime.fromtimestamp(float("1286715787.71")).strftime('%Y-%m-%d %H:%M:%S')
+    ident = models.IntegerField()
     timestamp = models.DateTimeField()
-    protocol = models.CharField(max_length=10)
+    protocol = models.IntegerField()
     src_ip = models.IPAddressField()
     dst_ip = models.IPAddressField()
     sport = models.IntegerField()
     dport = models.IntegerField()
     http = EmbeddedModelField('HttpDetails', null=True, blank=True)
 
+    def __unicode__(self):
+        return u'(%s, %s, %s, %s, %s)' % (self.protocol, self.src_ip, self.sport, self.dst_ip, self.dport)
+
 class HttpDetails(models.Model):
     # request or response
     http_type = models.CharField(max_length=10)
     # request fields
-    method = models.CharField(max_length=5)
-    uri = models.URLField()
-    headers = models.TextField()
-    version = models.IntegerField()
+    method = models.CharField(max_length=5, null=True, blank=True)
+    uri = models.URLField(null=True, blank=True)
+    headers = models.TextField(null=True, blank=True)
+    version = models.FloatField(null=True, blank=True)
     # request part ends
     # responde fields
     # header and version is here also
-    reason = models.CharField(max_length="5")
-    status = models.IntegerField()
-    body = models.TextField()
+    reason = models.CharField(max_length="5", null=True, blank=True)
+    status = models.IntegerField(null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
     # response ends
     files = ListField(null=True, blank=True)
