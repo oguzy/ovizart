889b7f93f9a8f8eba81c55ddf84476204f8455f4
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-05-27 22:53:57 +0300

    saving plain files added

--- openwitness/pcap/views.py
+++ openwitness/pcap/views.py
@@ -123,6 +123,7 @@ def upload(request):
                 # then call functions that will save request and responses that will parse dat files, save the headers and files
                 http_handler.save_request(upload_path, request.session['uploaded_hash'])
                 http_handler.save_response_headers(upload_path, request.session['uploaded_hash'])
+                http_handler.save_response_binaries(upload_path, request.session['uploaded_hash'])
                 http_handler.save_response_files(upload_path, request.session['uploaded_hash'])
                 # should save the file names to db also
 
