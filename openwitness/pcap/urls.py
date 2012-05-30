4d16b691a02ac5977b3a7668c52d22751c82ef07
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-05-30 23:01:08 +0300

    url fix for Django 1.3

--- openwitness/pcap/urls.py
+++ openwitness/pcap/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
-from pcap.views import *
+from openwitness.pcap.views import *
 
-urlpatterns = patterns('pcap.views',
+urlpatterns = patterns('',
     # upload page for pcap
-    url(r'^upload/$', view='upload', name='upload_pcap'),
+    url(r'^upload/', view=upload, name='upload_pcap'),
     )
