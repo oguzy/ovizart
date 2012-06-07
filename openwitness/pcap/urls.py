4f18fe28196950b421a76aa37039c88447ff1612
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-06-07 21:42:21 +0300

    summary page is defined

--- openwitness/pcap/urls.py
+++ openwitness/pcap/urls.py
@@ -4,4 +4,5 @@ from openwitness.pcap.views import *
 urlpatterns = patterns('',
     # upload page for pcap
     url(r'^upload/', view=upload, name='upload_pcap'),
+    url(r'^summary/', view=summary, name='summary_pcap'),
     )
