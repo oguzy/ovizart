bbd4d1133e3c73823a8ef5a56b925d14aac11625
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-06-10 14:58:32 +0300

    login form is defined with a custom validation

---
+++ openwitness/main/forms.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm
+from django import forms
+from main.models import UserProfile
+from django.contrib.auth.models import User
+import hashlib
+
+
+class LoginForm(ModelForm):
+    user_email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'user_email', 'password')
+
+    def clean(self):
+        data = self.cleaned_data
+        email = data.get('user_email')
+        hash = hashlib.sha1()
+        hash.update(email)
+        email_hash = hash.hexdigest()
+        username = self.cleaned_data.get('username')
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise forms.ValidationError(u'User does not exist.')
+        if email and UserProfile.objects.filter(user_email=email_hash, user=user).count():
+            return data
+        else:
+            raise forms.ValidationError(u'Profile for the user does not exist')
