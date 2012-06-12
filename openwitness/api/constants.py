802cd13cd06ea899d04928460c87ce8cfb598939
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-06-12 20:43:21 +0300

    defined circular icons

--- openwitness/api/constants.py
+++ openwitness/api/constants.py
@@ -1,2 +1,2 @@
 IMPORTANCE = {'smtp': 40, 'http': 40, 'dns': 40}
-ICONS = {'smtp': "square_blue.png", 'http':"square_orange.png", 'dns': "square_red.png"}
+ICONS = {'smtp': "circle_blue.png", 'http':"circle_orange.png", 'dns': "circle_red.png"}
