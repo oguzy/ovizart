d6d0f148a8b1d546d64d35c3c2d30f8ace437732
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-06-12 11:13:07 +0300

    explanation is added

--- openwitness/main/models.py
+++ openwitness/main/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    user_email = models.EmailField()
+    user_email = models.CharField(max_length=100) # sha1 hash value of the email
