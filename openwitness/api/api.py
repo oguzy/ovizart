2901b232c0df126b6dab472ae688f2523494c55d
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-06-16 20:49:25 +0300

    rest api resources for packet counts & packet sizes per flow is added

--- openwitness/api/api.py
+++ openwitness/api/api.py
@@ -1,7 +1,7 @@
 
 from tastypie.resources import ModelResource
 from openwitness.pcap.models import FlowDetails
-from openwitness.api.serializer import CustomJSONSerializer
+from openwitness.api.serializer import CustomJSONSerializer, AppProtocolPacketSizeCustomJSONSerializer, AppProtocolPacketCountCustomJSONSerializer
 from tastypie.constants import ALL
 
 class AppProtocolResource(ModelResource):
@@ -15,5 +15,32 @@ class AppProtocolResource(ModelResource):
             'user_id': ALL,
         }
         serializer = CustomJSONSerializer()
+
+class AppProtocolVisualizePacketSizeResource(ModelResource):
+    class Meta:
+        queryset = FlowDetails.objects.all()
+        resource_name = 'protocol'
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
+        limit = 0 # unlimited
+        filtering = {
+            'user_id': ALL,
+            'protocol': ALL,
+        }
+        serializer = AppProtocolPacketSizeCustomJSONSerializer()
+
+class AppProtocolVisualizePacketCountResource(ModelResource):
+    class Meta:
+        queryset = FlowDetails.objects.all()
+        resource_name = 'protocol'
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
+        limit = 0 # unlimited
+        filtering = {
+            'user_id': ALL,
+            'protocol': ALL,
+            }
+        serializer = AppProtocolPacketCountCustomJSONSerializer()
+
 
 
