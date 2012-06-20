9a8db3f3dee3aed5dc00f4511cbfb8a3b0ab523d
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-06-20 17:26:30 +0300

    url for the flow details is added

--- openwitness/pcap/urls.py
+++ openwitness/pcap/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^upload/', view=upload, name='upload_pcap'),
     url(r'^summary/', view=summary, name='summary_pcap'),
     url(r'^visualize/(?P<protocol>\w+)/(?P<type>\w+)/$', view=visualize, name='visualize_app_layer'),
+    url(r'^flow/(?P<flow_id>\w+)/$', view=flow_details, name='flow_details'),
     )
