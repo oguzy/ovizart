ef0506f83bb544186e3927f989f4bbb27f042e90
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-06-28 22:09:31 +0300

    API function for getting all protocol statistics is added

--- openwitness/api/api.py
+++ openwitness/api/api.py
@@ -1,7 +1,7 @@
 
 from tastypie.resources import ModelResource
 from openwitness.pcap.models import FlowDetails
-from openwitness.api.serializer import CustomJSONSerializer, AppProtocolPacketSizeCustomJSONSerializer, AppProtocolPacketCountCustomJSONSerializer
+from openwitness.api.serializer import CustomJSONSerializer, AppProtocolPacketSizeCustomJSONSerializer, AppProtocolPacketCountCustomJSONSerializer, AllProtocolsJSONSerializer
 from tastypie.constants import ALL
 
 class AppProtocolResource(ModelResource):
@@ -42,5 +42,12 @@ class AppProtocolVisualizePacketCountResource(ModelResource):
             }
         serializer = AppProtocolPacketCountCustomJSONSerializer()
 
-
+class AllProtocolsResource(ModelResource):
+    class Meta:
+        queryset = FlowDetails.objects.all()
+        resource_name = 'all_protocols'
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
+        limit = 0 # unlimited
+        serializer = AllProtocolsJSONSerializer()
 
