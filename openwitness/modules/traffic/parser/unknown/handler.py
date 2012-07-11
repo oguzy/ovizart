0767043884524edd712f58fa0dc05861d29a6744
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-07-11 16:29:25 +0300

    unknown traffic handling is done via reading Bro conn.log rading and comparing the results with db

--- openwitness/modules/traffic/parser/unknown/handler.py
+++ openwitness/modules/traffic/parser/unknown/handler.py
@@ -1,6 +1,7 @@
 from openwitness.modules.traffic.log.logger import Logger
 import os
 import datetime
+from openwitness.pcap.models import FlowDetails
 
 
 class Handler(object):
@@ -8,10 +9,10 @@ class Handler(object):
         super(Handler, self).__init__()
 
     def get_flow_ips(self, **args):
-        result = self.read_conn_log(args['path'])
+        result = self.read_conn_log(args['path'], args['parent_hash_value'], args['user_id'])
         return result
 
-    def read_conn_log(self, path):
+    def read_conn_log(self, path, parent_hash_value, user_id):
         result = [] # lets the keys the connection id, values the ts
         conn_log_path = os.path.join(path, "conn.log")
         f = open(conn_log_path, "r")
@@ -21,6 +22,13 @@ class Handler(object):
             tmp = info[2:6]
             dt = datetime.datetime.fromtimestamp(float(info[0]))
             tmp.append(dt)
-            result.append(dt)
+
+            check = FlowDetails.objects.filter(parent_hash_value=parent_hash_value, user_id=user_id, src_ip=tmp[0], sport=int(tmp[1]),
+                                        dst_ip=tmp[2], dport=int(tmp[3]))
+
+            if len(check,) > 0:
+                continue
+
+            result.append(tmp)
 
         return result
