98a63208861aca9e56d1eae844d046dac3e38c46
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-07-20 11:13:10 +0300

    url is defined to create the pcap link from pcap id

--- openwitness/pcap/urls.py
+++ openwitness/pcap/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^summary/', view=summary, name='summary_pcap'),
     url(r'^visualize/(?P<protocol>\w+)/(?P<type>\w+)/$', view=visualize, name='visualize_app_layer'),
     url(r'^flow/(?P<flow_id>\w+)/$', view=flow_details, name='flow_details'),
+    url(r'^get_pcap_url/(?P<id>\w+)/$', view=get_pcap_url, name='get_pcap_url'),
     )
