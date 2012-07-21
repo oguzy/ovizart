5bb58d4d198b615a34e3eaaccd8493fd598d4200
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-07-21 11:15:54 +0300

    url to map the packet detail view is aded

--- openwitness/pcap/urls.py
+++ openwitness/pcap/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^visualize/(?P<protocol>\w+)/(?P<type>\w+)/$', view=visualize, name='visualize_app_layer'),
     url(r'^flow/(?P<flow_id>\w+)/$', view=flow_details, name='flow_details'),
     url(r'^get_pcap_url/(?P<id>\w+)/$', view=get_pcap_url, name='get_pcap_url'),
+    url(r'^info/(?P<packet_ident>\w+)/$', view=get_packet_info, name='packet_info'),
     )
