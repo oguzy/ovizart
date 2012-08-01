501fe2fa5dc19a37d2f5f347022422db9d9ad544
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2012-08-01 21:32:36 +0300

    url space is defined to map clicked md5 at the main page to a view

--- openwitness/pcap/urls.py
+++ openwitness/pcap/urls.py
@@ -3,6 +3,7 @@ from openwitness.pcap.views import *
 
 urlpatterns = patterns('',
     # upload page for pcap
+    url(r'^(?P<flow_pcap_md5>\w+)/$', view=flow_pcap_details, name='flow_pcap_details'),
     url(r'^upload/', view=upload, name='upload_pcap'),
     url(r'^summary/', view=summary, name='summary_pcap'),
     url(r'^visualize/(?P<protocol>\w+)/(?P<type>\w+)/$', view=visualize, name='visualize_app_layer'),
